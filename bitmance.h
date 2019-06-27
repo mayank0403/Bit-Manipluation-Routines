fe22c5d2d4ce043c915c9dee343ea062621cf374
Mayank <mayankrathee.japan@gmail.com>
2019-06-27 18:10:45 +0530

    Added support for double

--- bitmance.h
+++ bitmance.h
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <stdlib.h>
+#include <cstring>
 
 using namespace std;
+
+// Use double precisions for floats
+#define DOUBLE
 
 void bit_manip_inside_byte(int bit /*0/1*/, int location, void* number){
 	if(location >= 8){
@@ -45,21 +49,46 @@ void print_bits_long_type(void* number, bool is_float = false){
 			return;
 		}
 		cout<<" | man (reverse of expected) => ";
-		for(int i=0; i<23; i++){
+		int i = 0;
+#ifdef DOUBLE
+		for(i=0; i<52; i++){
+			int bit = ((*((long long int*)number)) >> i) & 1UL; 
+			cout<<bit<<" ";
+		}
+
+#else
+		for(i=0; i<23; i++){
 			int bit = ((*((unsigned long*)number)) >> i) & 1UL; 
 			cout<<bit<<" ";
 		}
+#endif
 		cout<<". 1";
 		cout<<" | exp (parsed normally, no rev)=> ";
-		for(int i=0; i<8; i++){
+#ifdef DOUBLE
+		for(; i<63; i++){
+			int bit = ((*((long long int*)number)) >> i) & 1UL; 
+			cout<<bit<<" ";
+		}
+	
+#else
+		for(; i<31; i++){
 			int bit = ((*((unsigned long*)number)) >> i) & 1UL; 
 			cout<<bit<<" ";
 		}
+#endif
 		cout<<" | sign => ";
-		for(int i=0; i<1; i++){
+#ifdef DOUBLE
+		for(; i<64; i++){
+			int bit = ((*((long long int*)number)) >> i) & 1UL; 
+			cout<<bit<<" ";
+		}
+
+#else
+		for(; i<32; i++){
 			int bit = ((*((unsigned long*)number)) >> i) & 1UL; 
 			cout<<bit<<" ";
 		}
+#endif
 		cout<<" |"<<endl;
 
 		return;
@@ -70,7 +99,7 @@ void print_bits_long_type(void* number, bool is_float = false){
 	cout<<endl;
 }
 
-//Set/Unset nth bit (of 32)
+//Set/Unset nth bit (of 32/64)
 void bit_manip_long_type(int bit, int location, void* number){
 	int chunk_num = location/8;
 	int relative_location = location%8;
@@ -82,6 +111,7 @@ int extract_bit_from_byte(int position, void* number){
 	return ((*((unsigned long*)number)) >> position) & 1UL;  	
 }
 
+//For both double and float
 int extract_bit_from_long_type(int location, void* number){
 	int chunk_num = location/8;
 	int relative_location = location%8;
@@ -102,24 +132,55 @@ void set_x_bits_to_little_endian_float(int x, void* from, void* to){
 }
 
 //Generate a float value from integer representations of mantissa and exponent.
-void gen_float_from_man_exp(int sign /*1 -ve*/, int exponent /*<2pow8-- actual exponent = exponent-127*/, int mantissa /*<2pow23*/, void* number){
+void gen_float_from_man_exp(int sign /*1 -ve*/, long long int exponent /*<2pow8-- actual exponent = exponent-127*/, long long int mantissa /*<2pow23*/, void* number){
 	if(__BYTE_ORDER == __BIG_ENDIAN){
 		cout<<"BIG ENDIAN"<<endl;
+#ifdef DOUBLE
+		bit_manip_long_type(sign, 0, number); //Set sign.
+		set_x_bits_to(11, 1, &exponent, number);
+		set_x_bits_to(52, 12, &mantissa, number);
+
+#else
 		bit_manip_long_type(sign, 0, number); //Set sign.
 		set_x_bits_to(8, 1, &exponent, number);
 		set_x_bits_to(23, 9, &mantissa, number);
+#endif
 	}
 	else if(__BYTE_ORDER == LITTLE_ENDIAN){
 		cout<<"LITTLE ENDIAN"<<endl;
+#ifdef DOUBLE
+		set_x_bits_to_little_endian_float(52, &mantissa, number); //parsed in little endian form.
+		set_x_bits_to(11, 52, &exponent, number);
+		bit_manip_long_type(sign, 63, number); //Set sign.
+	
+#else
 		set_x_bits_to_little_endian_float(23, &mantissa, number); //parsed in little endian form.
 		set_x_bits_to(8, 23, &exponent, number);
 		bit_manip_long_type(sign, 31, number); //Set sign.
+#endif
 	}
 }
 
-float gen_random_valid_float(){
-	int random_sign, random_exponent, random_mantissa;
+//Works for both float and double
+void* gen_random_valid_float(){
+	long long int random_sign, random_exponent, random_mantissa;
 	random_sign = rand()%2; //1 bit
+#ifdef DOUBLE
+	random_exponent = rand()%(2*1024); //11 bits
+	while(random_exponent == 0 || random_exponent == (2*1024 - 1)){
+		//if 0, then either 0 or denormal/subnormal number
+		//if 2*1024-1, then chances are that it is NaN or inf
+		random_exponent = rand()%(2*1024); //8 bits
+	}
+	long long int modulus = (1024*1024*1024);
+	modulus *= 1024;
+       	modulus *= 1024*4;	
+	random_mantissa = rand()%(modulus); //52 bits
+	void* rand_double = malloc(8);
+	gen_float_from_man_exp(random_sign, random_exponent, random_mantissa, rand_double);
+	return rand_double;
+
+#else
 	random_exponent = rand()%(256); //8 bits
 	while(random_exponent == 0 || random_exponent == 255){
 		//if 0, then either 0 or denormal/subnormal number
@@ -127,8 +188,22 @@ float gen_random_valid_float(){
 		random_exponent = rand()%(256); //8 bits
 	}
 	random_mantissa = rand()%(1024*1024*8); //23 bits
-	float rand_float = 0.0;
-	gen_float_from_man_exp(random_sign, random_exponent, random_mantissa, &rand_float);
+	void* rand_float = malloc(4);
+	gen_float_from_man_exp(random_sign, random_exponent, random_mantissa, rand_float);
 	return rand_float;
+#endif
 }
 
+float get_random_float(){
+	float value = 0.0;
+	void* rand_val = gen_random_valid_float();
+	memcpy(&value, rand_val, 4);
+	return value;
+}
+
+double get_random_double(){
+	double value = 0.0;
+	void* rand_val = gen_random_valid_float();
+	memcpy(&value, rand_val, 8);
+	return value;
+}
--- example.cpp
+++ example.cpp
@@ -2,15 +2,23 @@
 
 int main(){
 	void* float_mem = malloc(4); //4 bytes -- size of float
+	void* double_mem = malloc(8); //8 bytes -- size of double
 	//print_bits_long_type(float_mem);
 	//bit_manip_inside_byte(1, 0, float_mem);
 	//bit_manip_inside_byte(1, 1, float_mem);
 	//bit_manip_long_type(1, 31, float_mem);
 	//print_bits_long_type(float_mem);
 	//cout<<*((int*)float_mem)<<endl;
-	gen_float_from_man_exp(0, 127, 3, float_mem);
-	cout<<*((float*)float_mem)<<endl;
-	print_bits_long_type(float_mem, true);
+	//gen_float_from_man_exp(1, 127, 3, float_mem); //127 bias
+	//gen_float_from_man_exp(1, 1023, 3, double_mem); //1023 bias
+	//double a = 1.5;
+	//print_bits_long_type(&a, true);
+	//cout<<*((float*)float_mem)<<endl;
+	//cout<<*((double*)double_mem)<<endl;
+	//print_bits_long_type(float_mem, true);
+	//print_bits_long_type(double_mem, true);
+	//for(int i=0; i<100; i++)
+		//cout<<get_random_float()<<endl;
 	for(int i=0; i<100; i++)
-		cout<<gen_random_valid_float()<<endl;
+		cout<<get_random_double()<<endl;
 }
